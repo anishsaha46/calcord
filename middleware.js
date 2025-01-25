1d69d3aeca353fd8d406e753fd4d7d7fa7818e22
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-01-25 10:02:28 +0530

    User_Authentication done

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = (path) => {
+    return [
+        "/dashboard",
+        "/events",
+        "/meetings",
+        "/availability"
+    ].some(route => path.startsWith(route));
+};
+
+export default clerkMiddleware((auth, req) => {
+    if (!auth().userId && isProtectedRoute(req.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL('/sign-in', req.url));
+    }
+});
+
+export const config = {
+    matcher: [
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/(api|trpc)(.*)',
+    ],
+};
